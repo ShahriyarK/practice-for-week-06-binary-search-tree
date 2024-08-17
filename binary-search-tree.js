916d3d8ca0810b01ebae4542b3308aa3e0362b9f
Shahriyar Kashif <shahriyar.k@hotmail.com>
2024-08-17 14:42:39 +0500

    Implement insert method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,35 +8,46 @@ class TreeNode {
 }
 
 class BinarySearchTree {
-
   constructor() {
-    // Your code here
+    this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
-    // Your code here
+  insert(val, currentNode = this.root) {
+    // check if the tree is empty
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+    // we will check subtrees recursively to identify where to insert the node
+    // base case
+    // we don't need to return anything explicitly as stackframes that pop will return
+    // undefined and thats fine we are only adding nodes recursively
+    if (val < currentNode.val) {
+      if (!currentNode.left) currentNode.left = new TreeNode(val);
+      else this.insert(val, currentNode.left);
+    } else {
+      if (!currentNode.right) currentNode.right = new TreeNode(val);
+      else this.insert(val, currentNode.right);
+    }
   }
 
   search(val) {
     // Your code here
   }
 
-
   preOrderTraversal(currentNode = this.root) {
     // Your code here
   }
-
 
   inOrderTraversal(currentNode = this.root) {
     // Your code here
   }
 
-
   postOrderTraversal(currentNode = this.root) {
     // Your code here
   }
 
-    // Breadth First Traversal - Iterative
+  // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
     // your code here
   }
@@ -44,7 +55,7 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
-}
+  }
 }
 
 module.exports = { BinarySearchTree, TreeNode };
