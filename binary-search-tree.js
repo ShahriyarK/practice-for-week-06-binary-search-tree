39672f823b828afba4682b56b6642268facd7db8
Shahriyar Kashif <shahriyar.k@hotmail.com>
2024-08-17 14:57:00 +0500

    Implement iterative approach for inserting node

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -14,20 +14,43 @@ class BinarySearchTree {
 
   insert(val, currentNode = this.root) {
     // check if the tree is empty
+    // if (!this.root) {
+    //   this.root = new TreeNode(val);
+    //   return;
+    // }
+    // // we will check subtrees recursively to identify where to insert the node
+    // // base case
+    // // we don't need to return anything explicitly as stackframes that pop will return
+    // // undefined and thats fine we are only adding nodes recursively
+    // if (val < currentNode.val) {
+    //   if (!currentNode.left) currentNode.left = new TreeNode(val);
+    //   else this.insert(val, currentNode.left);
+    // } else {
+    //   if (!currentNode.right) currentNode.right = new TreeNode(val);
+    //   else this.insert(val, currentNode.right);
+    // }
+
+
+    // Iterative approach
     if (!this.root) {
       this.root = new TreeNode(val);
       return;
     }
-    // we will check subtrees recursively to identify where to insert the node
-    // base case
-    // we don't need to return anything explicitly as stackframes that pop will return
-    // undefined and thats fine we are only adding nodes recursively
-    if (val < currentNode.val) {
-      if (!currentNode.left) currentNode.left = new TreeNode(val);
-      else this.insert(val, currentNode.left);
-    } else {
-      if (!currentNode.right) currentNode.right = new TreeNode(val);
-      else this.insert(val, currentNode.right);
+    const newNode = new TreeNode(val);
+    while (currentNode) {
+      if (val < currentNode.val) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        else currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        else currentNode = currentNode.right;
+      }
     }
   }
 
